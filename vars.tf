bb7844d786c18b752eab032fad7c1a451c8d4196
Jean Silva <jeans_bs@outlook.com>
2024-05-15 03:39:38 -0300

    add vars.tf

---
+++ vars.tf
@@ -0,0 +1,14 @@
+variable "accessConfig" {
+  default = "API_AND_CONFIG_MAP"
+}
+
+variable "aws_access_key_id" {
+  type = string
+  description = "AWS public key"
+}
+
+
+variable "aws_secret_access_key" {
+  type = string
+  description = "AWS secret key"
+}
